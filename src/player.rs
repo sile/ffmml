d0581c935a4f74e3ec4468ad73553e04b2d53a6e
Takeru Ohta <phjgt308@gmail.com>
2022-12-25 14:40:29 +0900

    Fix clippy(nightly) warnings

--- src/player.rs
+++ src/player.rs
@@ -144,7 +144,7 @@ impl PlayMusicError {
 
 impl std::fmt::Debug for PlayMusicError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self)
+        write!(f, "{self}")
     }
 }
 
